7701eca84c3b494605e7167fd5008b621cd959d4
Guilherme Crupi <guilhermecrupi@hotmail.com>
2022-04-06 18:41:03 -0300

    html-semantico

---
+++ Fundamentos/JavaScript/script.js
@@ -0,0 +1,10 @@
+const myName= "Guilherme Crupi de Moura Pereira";
+const birthCity= "Rio Claro"
+let birthYear= 1997
+
+birthYear= 2030;
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+
+
