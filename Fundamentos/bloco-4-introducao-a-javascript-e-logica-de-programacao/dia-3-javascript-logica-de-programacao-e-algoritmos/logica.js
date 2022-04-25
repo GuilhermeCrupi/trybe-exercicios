b1ad6b8440423e79ed4c3c9546377b2dc9626fa3
Guilherme Crupi <guilhermecrupi@hotmail.com>
2022-04-25 15:30:53 -0300

    Finalizando Exercicios

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logica.js
@@ -0,0 +1,6 @@
+let number = 4;
+let numberFator= [];
+for(index=0; index < number.length; index-=){
+    numberFator = number*number(index -=1)
+
+} 
---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-e- for/objetos-e-for-in.js
@@ -0,0 +1,45 @@
+        // Exercicios 1 a 5
+
+// let info = { 
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: 'Sim'
+//   };
+//   for(let index in info){
+//       console.log(info[index])
+//   }
+
+// console.log(info.recorrente)
+
+// let info = {
+//     personagem: 'Margarida e Tio Patinhas,',
+//     origem: 'Pato Donald e  Christmas on Bear Mountain, Dells Four Color Comics #178',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas',
+//     recorrente: 'Ambos recorrentes'  
+// };  
+//     for(let index in info){
+//         console.log(info[index]);
+//     }
+
+            // Exercicios 6 a 8
+
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: 
+//         {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+        
+//         },
+//         {
+//             titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//             autor: 'JK Rowling',
+//             editora: 'Rocco',
+//         },
+//     ],
+//     };
+//     console.log(leitor.nome + ' tem 2 livros favoritos, ' + leitor.livrosFavoritos[0].titulo + ' e ' + leitor.livrosFavoritos[1].titulo);
